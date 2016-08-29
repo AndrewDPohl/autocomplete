c972370a59145d5fcede88c8e04b1e9d741bd346
Andrew Pohl <andrewpohl@Andrews-MacBook-Pro.local>
2016-08-28 18:52:03 -0700

    Initial commit
    * Adding the code that I have so far
    * Autocomplete is working

---
+++ README.md
@@ -0,0 +1,3 @@
+# jQuery Autocomplete
+
+## An autocomplete that utilizes jQuery but without any specific autocomplete plugins.  
---
+++ main.js
@@ -0,0 +1,34 @@
+
+var getList = function() {
+  var items = [];
+  var currentStr = $("input").val();
+  // var re = /^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/\s\w?]*$/;
+  var re = /^[a-zA-Z0-9\s\w?]*$/i;
+  $('.list-item').remove();
+  $.getJSON( "./data.json", function( data ) {
+    for(i = 0; i < data.products.length; i++) {
+      var place = i;
+      var product = data.products[i].name;
+      var url = data.products[i].url;
+      var type = data.products[i].type;
+      if ( product.match(currentStr.match(re)) && currentStr !== "" ) {
+        items.push("<li id='" + place + "'>" + product + "</li>");
+        $('#my-new-list').append("<li class='list-item' id='" + place + "'>" + product + "</li>");
+        items = [];
+      } 
+    }
+    $(".list-item").click(function() {
+      var id = this.id;
+      $('#result').append("<li class='item-name'>Name: " + data.products[id].name + "</li>");
+      $('#result').append("<li>Website: <a href='" + data.products[id].url + "' class='item-url'>" + data.products[id].url + "</a></li>");
+      $('#result').append("<li class='item-type'>Product Type: " + data.products[id].type + "</li>");
+    });
+  });
+};
+
+$(document).ready(function(){
+    $("input").keyup(function(){
+      getList();
+    });
+});
+
